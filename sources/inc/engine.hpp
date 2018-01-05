8eb1898094f5f9d59844e7d7f90faba9611d9efa
Thomas RETY <thomas.rety@epitech.eu>
2018-01-05 10:57:57 +0100

    first commit

---
+++ sources/inc/engine.hpp
@@ -0,0 +1,12 @@
+//
+// EPITECH PROJECT, 2018
+// Header
+// File description:
+// hpp
+//
+
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+
+#endif /* _HEADER_H_ */
---
+++ sources/obj/main.o
@@ -0,0 +1,13 @@
+//
+// EPITECH PROJECT, 2018
+// test
+// File description:
+// test
+//
+
+#include <string>
+
+int main()
+{
+	return (1);
+}
